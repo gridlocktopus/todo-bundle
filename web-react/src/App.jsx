f9c7d26788c18c06f7cb5ae01461c95e47cce538
gridlocktopus <gridlocktopus@proton.me>
2026-02-06 00:20:20 +0100

    feat: save and load todos from localstorage

--- web-react/src/App.jsx
+++ web-react/src/App.jsx
@@ -1,46 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
+
+  // initialize the todos array
+  const [todos, setTodos] = useState(() => {
+    const stored = localStorage.getItem("savedTodos");
+    // return either stored todos or an empty array
+    return stored ? JSON.parse(stored) : [];
+  });
+  // initialize the user input text
   const [todoText, setTodoText] = useState("");
+
+  // Save todos to local storage
+  useEffect(() => {
+    localStorage.setItem("savedTodos", JSON.stringify(todos));
+  }, [todos]);
 
   function createTodo(todoText, todoDueAt) {
     return {
       id: crypto.randomUUID(),
       text: todoText,
       createdAt: Date.now(),
-      dueAt: todoDueAt || null
-    }
+      dueAt: todoDueAt || null,
+    };
   }
 
-  const handleTextInputChange = (e) => {setTodoText(e.target.value);};
+  const handleTextInputChange = (e) => {
+    setTodoText(e.target.value);
+  };
 
   const handleAddButton = () => {
-    if (todoText.trim() === ""){
+    if (todoText.trim() === "") {
       return;
     }
-  
+
     const newTodo = createTodo(todoText);
-    setTodos(prevTodos => [...prevTodos, newTodo]);
-    
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+
     setTodoText("");
-  }
+  };
 
   console.log(todos);
-
-   
 
   return (
     <>
       <div>
-        <input type='text' value={todoText} onChange={handleTextInputChange}></input>
+        <input
+          type="text"
+          value={todoText}
+          onChange={handleTextInputChange}
+        ></input>
         <button onClick={handleAddButton}>Add task</button>
         <ul>
-          {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
         </ul>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -62,7 +82,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
