baf832a9def467c96c205a8ae9c3e6e410446ad3
gridlocktopus <gridlocktopus@proton.me>
2026-02-05 01:29:59 +0100

    feat: add input field, button, and todo list rendering

--- web-react/src/App.jsx
+++ web-react/src/App.jsx
@@ -4,11 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  function createTodo(todoText, todoDueAt) {
+    return {
+      id: crypto.randomUUID(),
+      text: todoText,
+      createdAt: Date.now(),
+      dueAt: todoDueAt || null
+    }
+  }
+
+  const handleTextInputChange = (e) => {setTodoText(e.target.value);};
+
+  const handleAddButton = () => {
+    if (todoText.trim() === ""){
+      return;
+    }
+  
+    const newTodo = createTodo(todoText);
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+    
+    setTodoText("");
+  }
+
+  console.log(todos);
+
+   
 
   return (
     <>
       <div>
+        <input type='text' value={todoText} onChange={handleTextInputChange}></input>
+        <button onClick={handleAddButton}>Add task</button>
+        <ul>
+          {todos.map(todo => <li key={todo.id}>{todo.text}</li>)}
+        </ul>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
